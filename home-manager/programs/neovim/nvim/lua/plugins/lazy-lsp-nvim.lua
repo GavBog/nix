0fd77314bee43c89c3b6cba53468dda0d63ebda5
GavBog <me@gbogie.com>
2024-04-11 22:43:17 -0400

    Prefer locally installed servers over nix-shell

--- home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
@@ -3,7 +3,9 @@ return {
     "dundalek/lazy-lsp.nvim",
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
-      require("lazy-lsp").setup({})
+      require("lazy-lsp").setup({
+        prefer_local = true,
+      })
     end,
   },
 }
