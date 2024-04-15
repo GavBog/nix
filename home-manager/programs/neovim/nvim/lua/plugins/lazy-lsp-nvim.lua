eb648901d54f6e3c4e7c3667634e8f91126f5a81
GavBog <me@gbogie.com>
2024-04-15 12:29:42 -0400

    Add Preferred LSPs

--- home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
@@ -5,6 +5,32 @@ return {
     config = function()
       require("lazy-lsp").setup({
         prefer_local = true,
+
+        excluded_servers = {
+          "diagnosticls",
+          "efm",
+          -- Bugged servers
+          "sqls",
+          "rome",
+        },
+        preferred_servers = {
+          c = { "clangd" },
+          cpp = { "clangd" },
+          cs = { "omnisharp" },
+          go = { "gopls" },
+          haskell = { "hls" },
+          javascript = { "tsserver" },
+          javascriptreact = { "tsserver" },
+          json = { "jsonls" },
+          jsx = { "tsserver" },
+          nix = { "nil_ls" },
+          python = { "pyright" },
+          rust = { "rust_analyzer" },
+          tsx = { "tsserver" },
+          typescript = { "tsserver" },
+          typescriptreact = { "tsserver" },
+          yaml = { "yamlls" },
+        },
       })
     end,
   },
