208538c58158b43520b66e8eaf3007b59ba62025
GavBog <me@gbogie.com>
2024-05-23 16:58:45 -0400

    Lazy Load LSPs and Add Clippy Linter for Rust

--- home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/lazy-lsp-nvim.lua
@@ -2,29 +2,11 @@ return {
   {
     "dundalek/lazy-lsp.nvim",
     dependencies = {
-      "neovim/nvim-lspconfig",
-      { "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/nvim-cmp",
+      "VonHeikemen/lsp-zero.nvim",
     },
+    lazy = true,
+    event = "LazyFile",
     config = function()
-      local lsp_zero = require("lsp-zero")
-
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings to learn the available actions
-        lsp_zero.default_keymaps({
-          buffer = bufnr,
-          preserve_mappings = false
-        })
-
-        -- set keymaps
-        vim.keymap.set('n', '<leader>cf', '<cmd>LspZeroFormat!<cr>', { buffer = bufnr })
-        vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { desc = "Lsp Info" })
-
-        -- enable formatting on save
-        lsp_zero.buffer_autoformat()
-      end)
-
       require("lazy-lsp").setup {
         prefer_local = true,
 
@@ -53,7 +35,49 @@ return {
           typescriptreact = { "tsserver" },
           yaml = { "yamlls" },
         },
+
+        configs = {
+          rust_analyzer = {
+            settings = {
+              ["rust-analyzer"] = {
+                checkOnSave = {
+                  command = "clippy",
+                },
+              },
+            },
+          },
+        }
       }
     end,
   },
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/nvim-cmp",
+    },
+    lazy = true,
+    event = "LazyFile",
+
+    config = function()
+      local lsp_zero = require("lsp-zero")
+
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({
+          buffer = bufnr,
+          preserve_mappings = false
+        })
+
+        -- set keymaps
+        vim.keymap.set('n', '<leader>cf', '<cmd>LspZeroFormat!<cr>', { buffer = bufnr })
+        vim.keymap.set('n', '<leader>cl', '<cmd>LspInfo<cr>', { desc = "Lsp Info" })
+
+
+        -- enable formatting on save
+        lsp_zero.buffer_autoformat()
+      end)
+    end,
+  }
 }
---
+++ home-manager/programs/neovim/nvim/lua/plugins/noice-nvim.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      -- I will eventually find a better solution for this
+      "dundalek/lazy-lsp.nvim"
+    },
+  }
+}
