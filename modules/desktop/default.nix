69a0baf57749920319de675aa6d86b0805245086
GavBog <me@gbogie.com>
2025-10-23 20:35:26 -0400

    fix(greetd): Don't require greetd restart to update DWL

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,10 +1,11 @@
 { pkgs, customPkgs, ... }:
 {
+  environment.etc."dwl-start".source = "${customPkgs.dwl}/bin/dwl-start";
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd ${customPkgs.dwl}/bin/dwl-start";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd /etc/dwl-start";
         user = "greeter";
       };
     };
