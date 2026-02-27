da78df453ea9b0dae0804ac051aeb2bc41ee25d8
GavBog <me@gbogie.com>
2026-02-27 10:17:25 -0500

    fix(scx): fix my typo and switch to bpfland

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -4,14 +4,16 @@
   services.cloudflare-warp.enable = true;
   services.usbmuxd.enable = true;
   services.dbus.implementation = "broker";
-  services.scx.package = {
+  services.scx = {
     enable = true;
     package = pkgs.scx.rustscheds.overrideAttrs (old: {
+      cargoBuildFlags = [ "-p scx_bpfland" ];
+      cargoTestFlags = [ "-p scx_bpfland" ];
       meta = old.meta or { } // {
         badPlatforms = [ ];
       };
     });
-    scheduler = "scx_rustland";
+    scheduler = "scx_bpfland";
   };
   services.resolved.enable = true;
 
