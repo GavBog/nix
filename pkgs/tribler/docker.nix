0ba8dc4a3b54b68cb35b8dd4df5bb1789a7d37c6
GavBog <me@gbogie.com>
2026-03-14 20:11:20 -0400

    feat(tribler-docker): enable socat routing

--- pkgs/tribler/docker.nix
+++ pkgs/tribler/docker.nix
@@ -22,6 +22,9 @@ pkgs.dockerTools.buildLayeredImage {
       "QT_QPA_PLATFORM=offscreen"
       "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
 
+      "USER=user"
+      "HASHED_PASSWORD=replace-me"
+
       "EXTERNAL_IP=0.0.0.0"
       "IPV8_INT_PORT=8090"
       "LIBTORRENT_INT_PORT=20456"
@@ -41,11 +44,14 @@ pkgs.dockerTools.buildLayeredImage {
     Cmd = [
       "${pkgs.writeShellScript "entrypoint" ''
         mkdir -p /data/.Tribler/8.0/ /data/Downloads
-        # export LIBTORRENT_EXT_PORT=
-        # export IPV8_EXT_PORT=
-        # socat TCP4-LISTEN:$LIBTORRENT_INT_PORT,fork,reuseaddr TCP4:127.0.0.1:$LIBTORRENT_EXT_PORT &
-        # socat UDP4-LISTEN:$LIBTORRENT_INT_PORT,fork,reuseaddr UDP4:127.0.0.1:$LIBTORRENT_EXT_PORT &
-        # socat UDP4-LISTEN:$IPV8_INT_PORT,fork,reuseaddr UDP4:127.0.0.1:$IPV8_EXT_PORT &
+        if [ "$LIBTORRENT_INT_PORT" != "$LIBTORRENT_EXT_PORT" ]; then
+          socat TCP4-LISTEN:$LIBTORRENT_INT_PORT,fork,reuseaddr TCP4:127.0.0.1:$LIBTORRENT_EXT_PORT &
+          socat UDP4-LISTEN:$LIBTORRENT_INT_PORT,fork,reuseaddr UDP4:127.0.0.1:$LIBTORRENT_EXT_PORT &
+        fi
+
+        if [ "$IPV8_INT_PORT" != "$IPV8_EXT_PORT" ]; then
+          socat UDP4-LISTEN:$IPV8_INT_PORT,fork,reuseaddr UDP4:127.0.0.1:$IPV8_EXT_PORT &
+        fi
 
         envsubst < ${triblerConfTemplate} > /data/.Tribler/8.0/configuration.json
 
