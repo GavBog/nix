0cdf147b8ccb3ed8e4c112942c3e8c88e466e846
GavBog <me@gbogie.com>
2026-03-21 20:15:24 -0400

    fix(kanata): default input

--- modules/input/default.nix
+++ modules/input/default.nix
@@ -9,7 +9,7 @@
     KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
   '';
 
-  systemd.services.kanata-internalKeyboard.serviceConfig = {
+  systemd.services.kanata-default.serviceConfig = {
     SupplementaryGroups = [
       "input"
       "uinput"
@@ -19,12 +19,7 @@
   services.kanata = {
     enable = true;
     keyboards = {
-      internalKeyboard = {
-        devices = [
-          # Replace the paths below with the appropriate device paths for your setup.
-          # Use `ls /dev/input/by-path/` to find your keyboard devices.
-          "/dev/input/by-path/platform-23510c000.spi-cs-0-event-kbd"
-        ];
+      default = {
         extraDefCfg = "process-unmapped-keys yes";
         config = ''
           (defsrc
