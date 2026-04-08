GavBog/nix

commit 516af64d4f640cdf8e502666a00a3719b9bc13f5

fix(nvim): nixpkgs upstream treesitter

pkgs/nvim/default.nix
@@ -4,7 +4,6 @@
   nixCats,
   tidal-cycles,
   neovim-nightly-overlay,
-  nvim-treesitter-main,
 }:
 let
   inherit (nixCats) utils;
@@ -42,7 +41,6 @@ let
       # (utils.fixSystemizedOverlay inputs.codeium.overlays
       #   (system: inputs.codeium.overlays.${system}.default)
       # )
-      nvim-treesitter-main.overlays.default
     ];
 
   # see :help nixCats.flake.outputs.categories

pkgs/default.nix
@@ -28,7 +28,6 @@
           nixCats
           tidal-cycles
           neovim-nightly-overlay
-          nvim-treesitter-main
           ;
       };
     in

flake.nix
@@ -22,10 +22,6 @@
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nvim-treesitter-main = {
-      url = "github:iofq/nvim-treesitter-main";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     tidal-cycles = {
       url = "github:mitchmindtree/tidalcycles.nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,7 +38,6 @@
       nix-index-database,
       sops-nix,
       neovim-nightly-overlay,
-      nvim-treesitter-main,
       tidal-cycles,
       ...
     }@inputs:

commit 1dcb40a3a9a6aa978a3560da9a4c734764238272

fix(nvim): allow impure treesitter grammars for now
This will be reverted once I fix treesitter (broke in commit 516af64)

pkgs/nvim/default.nix
@@ -116,7 +116,6 @@ let
           LazyVim
           nvim-lspconfig
           nvim-navic
-          nvim-treesitter.withAllGrammars
           nvim-treesitter-textobjects
           nvim-ts-autotag
           nvim-ts-context-commentstring