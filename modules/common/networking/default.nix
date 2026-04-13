8462f056b12da80cfc61e724afd4e13ee7ac8792
Gavin Bogie <me@gbogie.com>
2026-04-12 22:33:01 -0400

    fix(network): cloudflare-warp back on nixpkgs unstable

--- modules/common/networking/default.nix
+++ modules/common/networking/default.nix
@@ -1,9 +1,9 @@
-{ stablePkgs, ... }:
+{ ... }:
 {
-  services.cloudflare-warp = {
-    enable = true;
-    package = stablePkgs.cloudflare-warp;
-  };
+  services.cloudflare-warp.enable = true;
+  # https://github.com/NixOS/nixpkgs/issues/504119
+  networking.firewall.checkReversePath = "loose";
+
   services.resolved.enable = true;
   networking = {
     networkmanager.enable = true;
