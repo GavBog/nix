95a81a4bda77013ebe56de8495ca8ec99df6ac01
GavBog <me@gbogie.com>
2026-07-10 16:52:17 -0400

    feat(x86): swap nvidia drivers

--- systems/x86/configuration.nix
+++ systems/x86/configuration.nix
@@ -1,10 +1,21 @@
 {
+  config,
   ...
 }:
 {
   imports = [
     ./hardware-configuration.nix
   ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+  };
 
   system.stateVersion = "25.11";
 }
